ffa6e6b38f343426278a23e3fda5a6eb342dd018
Jerahmie Radder <jerahmie@gmail.com>
2025-06-01 11:09:12 -0500

    Added remaining device properties.

--- 01_device_management/gpu_query/src/hello_gpu.cu
+++ 01_device_management/gpu_query/src/hello_gpu.cu
@@ -38,7 +38,75 @@ extern "C" {
         printf(" Max Texture 1D: %d\n", pdevprop->maxTexture1D);
         printf(" Max Texture 1D Mipmap: %d\n", pdevprop->maxTexture1DMipmap);
         printf(" Max Surface 1D Linear: %d\n", pdevprop->maxTexture1DLinear);
-
+        printf(" Max Texture 2D: %d, %d\n", pdevprop->maxTexture2D[0],
+                                            pdevprop->maxTexture2D[1]);
+        printf(" Max Texture 2D Mipmap: %d, %d\n", pdevprop->maxTexture2DMipmap[0],
+                                                   pdevprop->maxTexture2DMipmap[1]);
+        printf(" Max Texture 2D Linear: %d, %d, %d\n", pdevprop->maxTexture2DLinear[0],
+                                                       pdevprop->maxTexture2DLinear[1],
+                                                       pdevprop->maxTexture2DLinear[2]);
+        printf(" Max Texture 2D Gather: %d, %d\n", pdevprop->maxTexture2DGather[0],
+                                                   pdevprop->maxTexture2DGather[1]);
+        printf(" Max Texture 3D: %d, %d, %d\n", pdevprop->maxTexture3D[0],
+                                                pdevprop->maxTexture3D[1],
+                                                pdevprop->maxTexture3D[2]);
+        printf(" Max Texure 3D Alt: %d, %d, %d\n", pdevprop->maxTexture3DAlt[0],
+                                                   pdevprop->maxTexture3DAlt[1],
+                                                   pdevprop->maxTexture3DAlt[2]);
+        printf(" Max Texture Cubemap: %d\n", pdevprop->maxTextureCubemap);
+        printf(" Max Texture 1D Layered: %d, %d\n", pdevprop->maxTexture1DLayered[0],
+                                                    pdevprop->maxTexture1DLayered[1]);
+        printf(" Max Texture 2D Layered: %d, %d, %d\n", pdevprop->maxTexture2DLayered[0],
+                                                        pdevprop->maxTexture2DLayered[1],
+                                                        pdevprop->maxTexture2DLayered[2]);
+        printf(" Max Texture Cubemap Layered: %d, %d\n", pdevprop->maxTextureCubemapLayered[0],
+                                                         pdevprop->maxTextureCubemapLayered[1]);
+        printf(" Max Surface 1D: %d\n", pdevprop->maxSurface1D);
+        printf(" Max Surface 2D: %d, %d\n", pdevprop->maxSurface2D[0],
+                                            pdevprop->maxSurface2D[1]);
+        printf(" Max Surface 3D: %d, %d, %d\n", pdevprop->maxSurface3D[0],
+                                                pdevprop->maxSurface3D[1],
+                                                pdevprop->maxSurface3D[2]);
+        printf(" Max Surface 1D Layered: %d, %d\n", pdevprop->maxSurface1DLayered[0],
+                                                    pdevprop->maxSurface1DLayered[1]);
+        printf(" Max Surface 2D Layered: %d, %d, %d\n", pdevprop->maxSurface2DLayered[0],
+                                                        pdevprop->maxSurface2DLayered[1],
+                                                        pdevprop->maxSurface2DLayered[2]);
+        printf(" Max Surface Cubemap: %d\n", pdevprop->maxSurfaceCubemap);
+        printf(" Max Surface Cubemap Layered: %d, %d\n", pdevprop->maxSurfaceCubemapLayered[0],
+                                                         pdevprop->maxSurfaceCubemapLayered[1]);
+        printf(" Surface Alignment: %lu\n", pdevprop->surfaceAlignment);
+        printf(" Concurrent Kernels: %d\n", pdevprop->concurrentKernels);
+        printf(" ECC Enabled: %d\n", pdevprop->ECCEnabled);
+        printf(" PCI Bus ID: %d\n", pdevprop->pciBusID);
+        printf(" PCI Device ID: %d\n", pdevprop->pciDeviceID);
+        printf(" PCI Domain ID: %d\n", pdevprop->pciDomainID);
+        printf(" TCC Driver: %d\n", pdevprop->tccDriver);
+        printf(" Async Engine Count: %d\n", pdevprop->asyncEngineCount);
+        printf(" Unified Addressing: %d\n", pdevprop->unifiedAddressing);
+        printf(" Memory Clock Rate: %d\n", pdevprop->memoryClockRate);
+        printf(" Memory Bus Width: %d\n", pdevprop->memoryBusWidth);
+        printf(" L2 Cache Size: %d\n", pdevprop->l2CacheSize);
+        printf(" Persisting L2 Cache Max Size: %d\n", pdevprop->persistingL2CacheMaxSize);
+        printf(" Max Thread Per Multiprocessor: %d\n", pdevprop->maxThreadsPerMultiProcessor);
+        printf(" Stream Priorities Supported: %d\n", pdevprop->streamPrioritiesSupported);
+        printf(" Global L1 Cache Supported: %d\n", pdevprop->globalL1CacheSupported);
+        printf(" Local L1 Cache Supported: %d\n", pdevprop->localL1CacheSupported);
+        printf(" Shared Mem Per Multiprocessor: %lu\n", pdevprop->sharedMemPerMultiprocessor);
+        printf(" Registers Per Multiprocessor: %d\n", pdevprop->regsPerMultiprocessor);
+        printf(" Managed Memory: %d\n", pdevprop->managedMemory);
+        printf(" Is Multi GPU Board: %d\n", pdevprop->isMultiGpuBoard);
+        printf(" Multi-GPU Board Group ID: %d\n", pdevprop->multiGpuBoardGroupID);
+        printf(" Single to Double Precision Perf Ratio: %d\n", pdevprop->singleToDoublePrecisionPerfRatio);
+        printf(" Pageable Memory Access: %d\n", pdevprop->pageableMemoryAccess);
+        printf(" Concurrent Managed Access: %d\n", pdevprop->concurrentManagedAccess);
+        printf(" Compute Preemption Supported: %d\n", pdevprop->computePreemptionSupported);
+        printf(" Can Use Host Pointer for Registered Memory: %d\n", pdevprop->canUseHostPointerForRegisteredMem);
+        printf(" Cooperative Launch: %d\n", pdevprop->cooperativeLaunch);
+        printf(" Cooperative Multi-Device Launch %d\n", pdevprop->cooperativeMultiDeviceLaunch);
+        printf(" Pageable Memory access Uses Host Page Tables: %d\n", pdevprop->pageableMemoryAccessUsesHostPageTables);
+        printf(" Direct Managed Memory Access From Host: %d\n", pdevprop->directManagedMemAccessFromHost);
+        printf(" Access Policy Max Windows Size: %d\n", pdevprop->accessPolicyMaxWindowSize);
         return 0;
     }
 
